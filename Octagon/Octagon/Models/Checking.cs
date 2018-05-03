9fde1ecf72f73179b752b73ee5688091e5ff4196
sirarthurnell <sirarthurnell@gmail.com>
2018-05-03 19:02:37 +0200

    Preparing models.

---
+++ Octagon/Octagon/Models/Checking.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Octagon.Models
+{
+    /// <summary>
+    /// Represents the direction of the
+    /// checking.
+    /// </summary>
+    public enum CheckingDirection
+    {
+        /// <summary>
+        /// Checking of entrance.
+        /// </summary>
+        In,
+
+        /// <summary>
+        /// Checking of exit.
+        /// </summary>
+        Out
+    }
+
+    /// <summary>
+    /// Represents a checking 
+    /// (checking in or checking out).
+    /// </summary>
+    public class Checking
+    {
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Time at which the checking was done.
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// Direction of the checking.
+        /// </summary>
+        public CheckingDirection Direction { get; set; }
+    }
+}
