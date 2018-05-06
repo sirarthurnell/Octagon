1ccb9b5ec68493a6b85b3da89aaa3c593e32f2eb
sirarthurnell <sirarthurnell@gmail.com>
2018-05-06 13:01:02 +0200

    Added more classes to represent worked time.

--- Octagon/Octagon/Models/WorkingDay.cs
+++ Octagon/Octagon/Models/WorkingDay.cs
@@ -15,11 +15,46 @@ namespace Octagon.Models
         public List<Checking> Checkings { get; private set; }
 
         /// <summary>
+        /// Gets the year of the day.
+        /// </summary>
+        public int Year { get; private set; }
+
+        /// <summary>
+        /// Gets the month of the day.
+        /// </summary>
+        public int Month { get; private set; }
+
+        /// <summary>
+        /// Gets the day number.
+        /// </summary>
+        public int Day { get; private set; }
+
+        /// <summary>
+        /// Gets the date at which the day starts.
+        /// </summary>
+        public DateTime DayStart { get; private set; }
+
+        /// <summary>
+        /// Gets the date at which the day ends.
+        /// </summary>
+        public DateTime DayEnd { get; private set;  }
+
+        /// <summary>
         /// Creates a new instance of WorkingDay.
         /// </summary>
-        public WorkingDay()
+        /// <param name="year">Year of the day.</param>
+        /// <param name="month">Month of the day.</param>
+        /// <param name="day">Number of the day.</param>
+        public WorkingDay(int year, int month, int day)
         {
             Checkings = new List<Checking>();
+            Year = year;
+            Month = month;
+            Day = day;
+
+            DayStart = new DateTime(year, month, day, 0, 0, 0);
+            var almost24Hours = new TimeSpan(0, 0, 59, 59, 99);
+            DayEnd = DayStart.Add(almost24Hours);
         }
 
         /// <summary>
---
+++ Octagon/Octagon/Models/WorkingMonth.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Octagon.Models
+{
+    public class WorkingMonth
+    {
+    }
+}
